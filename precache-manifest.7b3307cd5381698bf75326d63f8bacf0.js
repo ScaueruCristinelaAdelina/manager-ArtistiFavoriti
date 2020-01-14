5349b5018b3e4b03affcf5ad95c57c238c1653c2
ScaueruCristinelaAdelina <57981612+ScaueruCristinelaAdelina@users.noreply.github.com>
2020-01-14 05:03:21 +0200

    build

---
+++ precache-manifest.7b3307cd5381698bf75326d63f8bacf0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a6408a2e89a4bf18f0a782e181f6522",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f74808538039ad1f2e3",
+    "url": "/static/css/2.50b51f35.chunk.css"
+  },
+  {
+    "revision": "56b26b0f530b2e250b86",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1f74808538039ad1f2e3",
+    "url": "/static/js/2.67e4e3e0.chunk.js"
+  },
+  {
+    "revision": "b15a737ad2cfd16685fd27e8e85315d2",
+    "url": "/static/js/2.67e4e3e0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "56b26b0f530b2e250b86",
+    "url": "/static/js/main.e7099f9a.chunk.js"
+  },
+  {
+    "revision": "7c5d91fa33acde0d1cbd",
+    "url": "/static/js/runtime-main.7dc0140f.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
